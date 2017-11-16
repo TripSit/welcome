93cb09f539c879c97001f0b4b8ba42da4f61524c
Luke Slater <tinmachin3@gmail.com>
2017-11-16 11:15:03 +0000

    fixes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
+var request = require('request');
+var _ = require('underscore')._;
 var router = express.Router();
+var Moniker = require('moniker');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+console.log(req.headers);
+  request.get('http://tripbot.tripsit.me/api/cspeed/getCounts', {
+    'json': true
+  }, function(request, response, body) {
+    var leastCount = Infinity;
+    var leastChan = '#tripsit';
+    _.each(body.data[0], function(count, chan) {
+      if(count < leastCount) {
+        leastChan = chan.split('.')[1];
+        leastCount = count;
+      }
+    });
+    res.render('index', { title: 'Express', achan: leastChan, anick: Moniker.choose() });
+  });
 });
 
 module.exports = router;
