4aee803eba9eb0502d3cb51d0ce7afa8a6ea7f68
powerjungle <fabsidereal@gmail.com>
2021-03-05 15:05:56 +0100

    New package.json and fixed response error

--- routes/index.js
+++ routes/index.js
@@ -4,28 +4,35 @@ var _ = require('underscore')._;
 var router = express.Router();
 var Moniker = require('moniker');
 
+var leastCount = Infinity;
+var leastChan = '#tripsit';
+
 /* GET home page. */
 router.get('/', function(req, res) {
 console.log(req.headers);
-  request.get('http://tripbot.tripsit.me/api/cspeed/getCounts', {
+  //todo: request is deprecated: https://www.npmjs.com/package/request
+  request.get('https://tripbot.tripsit.me/api/cspeed/getCounts', {
     'json': true
   }, function(request, response, body) {
-    if (response.statusCode == 201 || response.statusCode == 200) {
-    var leastCount = Infinity;
-    var leastChan = '#tripsit';
-    _.each(body.data[0], function(count, chan) {
-      if(count < leastCount) {
-        leastChan = chan.split('.')[1];
-        leastCount = count;
-      }
-    });
+    if (response) {
+        if (response.statusCode == 201 || response.statusCode == 200) {
+        _.each(body.data[0], function(count, chan) {
+          if(count < leastCount) {
+            leastChan = chan.split('.')[1];
+            leastCount = count;
+          }
+        });
+
+        } else {
+          console.log('error conecting to API: ' + response.statusCode);
+        }
 
     } else {
-      console.log('error conecting to API: ' + response.statusCode);
+        console.log('no response received from request: ' + request);
     }
+
     res.render('index', { title: 'Express', achan: leastChan, anick: Moniker.choose() });
   });
 });
 
 module.exports = router;
-
